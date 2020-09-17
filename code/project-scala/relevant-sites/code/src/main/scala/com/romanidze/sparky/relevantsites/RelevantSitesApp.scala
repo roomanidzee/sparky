f9c13b52b03187d3eaaf4b4b0b751f4e25ca4ce5
Andrey Romanov <steampart@gmail.com>
2020-09-17 10:54:07 +0300

    relevant sites count

--- code/project-scala/relevant-sites/code/src/main/scala/com/romanidze/sparky/relevantsites/RelevantSitesApp.scala
+++ code/project-scala/relevant-sites/code/src/main/scala/com/romanidze/sparky/relevantsites/RelevantSitesApp.scala
@@ -3,6 +3,7 @@ package com.romanidze.sparky.relevantsites
 import com.romanidze.sparky.relevantsites.classes.Record
 import com.romanidze.sparky.relevantsites.processing.DataLoader
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -22,13 +23,33 @@ object RelevantSitesApp {
 
     import spark.implicits._
 
+    val rawAutos: DataFrame = spark.read.json("/labs/laba02/autousers.json")
+    val autoDF: DataFrame = rawAutos.select(explode(rawAutos("autousers")))
+                                    .toDF("uid")
+
+    autoDF.createOrReplaceTempView("auto_df")
+
     val rawData: RDD[Record] = sc.textFile("/labs/laba02/logs")
                                  .map(elem => DataLoader.processRecord(elem))
                                  .filter(elem => !( uidCond.apply(elem) || urlCond.apply(elem) ) )
 
     val recordDFData: DataFrame = rawData.toDF()
 
-    val auto = spark.read.json("/labs/laba02/autousers.json")
+    recordDFData.createOrReplaceTempView("record_data")
+
+    val recordBinaryDF: DataFrame = spark.sql(
+      """
+        |SELECT url
+        |FROM record_data records
+        |LEFT JOIN auto_df autos ON (records.uid=autos.uid)
+        |""".stripMargin
+    )
+      .withColumn("auto_flag", when(col("autos.uid").isNull, 0).otherwise(1))
+
+    val dfSize: Long = recordBinaryDF.count()
+
+
+
 
   }
 
