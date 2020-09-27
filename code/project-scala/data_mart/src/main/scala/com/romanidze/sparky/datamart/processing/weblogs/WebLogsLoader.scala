0522b0f84ead2ddf7cea206a4bbf772b4daf9b88
Andrey Romanov <steampart@gmail.com>
2020-09-27 13:08:53 +0300

    data_mart parsing urls

--- code/project-scala/data_mart/src/main/scala/com/romanidze/sparky/datamart/processing/weblogs/WebLogsLoader.scala
+++ code/project-scala/data_mart/src/main/scala/com/romanidze/sparky/datamart/processing/weblogs/WebLogsLoader.scala
@@ -1,7 +1,8 @@
 package com.romanidze.sparky.datamart.processing.weblogs
 
 import com.romanidze.sparky.datamart.processing.SchemaProvider
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
 
 class WebLogsLoader(implicit spark: SparkSession) {
 
@@ -12,6 +13,24 @@ class WebLogsLoader(implicit spark: SparkSession) {
       .parquet(path)
       .na
       .drop("all")
+
+  }
+
+  private def processURL(rawURL: Column): Column = {
+
+    val parsedURL: Column = callUDF("parse_url", rawURL, lit("HOST"))
+
+    regexp_replace(parsedURL, lit("www."), lit(""))
+
+  }
+
+  def processLogsDataFrame(input: DataFrame): DataFrame = {
+
+    input
+      .select(col("uid"), explode(col("visits")).alias("exploded_visits"))
+      .select(col("uid"), processURL(col("exploded_visits.url")).alias("url"))
+      .groupBy(col("uid"), col("url"))
+      .agg(count(col("url")).alias("url_count"))
 
   }
 
--- code/project-scala/data_mart/src/test/scala/com/romanidze/sparky/datamart/processing/weblogs/WebLogsLoaderTest.scala
+++ code/project-scala/data_mart/src/test/scala/com/romanidze/sparky/datamart/processing/weblogs/WebLogsLoaderTest.scala
@@ -3,26 +3,26 @@ package com.romanidze.sparky.datamart.processing.weblogs
 import java.net.{URI, URL}
 
 import com.holdenkarau.spark.testing.{HDFSCluster, SharedSparkContext}
+import com.romanidze.sparky.datamart.processing.SchemaProvider
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class WebLogsLoaderTest extends AnyFlatSpec with Matchers with SharedSparkContext {
+class WebLogsLoaderTest extends AnyWordSpec with Matchers with SharedSparkContext {
 
   var hdfsCluster: HDFSCluster = null
+  var workingDir: Path = null
 
   override def beforeAll(): Unit = {
     super.beforeAll()
     hdfsCluster = new HDFSCluster
     hdfsCluster.startHDFS()
-  }
-
-  it should "correctly load web logs by schema" in {
 
     val fs: FileSystem = FileSystem.get(new URI(hdfsCluster.getNameNodeURI()), new Configuration())
-    val workingDir: Path = fs.getWorkingDirectory
+    workingDir = fs.getWorkingDirectory
 
     val testDir: Path = new Path(workingDir, "test")
     fs.mkdirs(testDir)
@@ -30,18 +30,47 @@ class WebLogsLoaderTest extends AnyFlatSpec with Matchers with SharedSparkContex
     val resourceURL: URL = getClass.getClassLoader.getResource("test.parquet")
     fs.copyFromLocalFile(new Path(resourceURL.toURI), new Path(testDir, "test.parquet"))
 
-    implicit val spark: SparkSession = SparkSession.builder.config(sc.getConf).getOrCreate()
+  }
 
-    val logsLoader: WebLogsLoader = new WebLogsLoader()
-    val logsDF: DataFrame = logsLoader.getWebLogsDataFrame(s"${workingDir}/test")
+  "WebLogsLoader" should {
 
-    //проверить визуально, что с схемой всё хорошо
-    logsDF.printSchema()
-    logsDF.show(1, 200, vertical = true)
+    "correctly load web logs by schema" in {
 
-    logsDF.count() > 0 shouldBe true
+      implicit val spark: SparkSession = SparkSession.builder.config(sc.getConf).getOrCreate()
 
-    spark.stop()
+      val logsLoader: WebLogsLoader = new WebLogsLoader()
+
+      val logsDF: DataFrame = logsLoader.getWebLogsDataFrame(s"${workingDir}/test")
+
+      logsDF.count() > 0 shouldBe true
+
+      logsDF.schema shouldBe SchemaProvider.getWebLogsSchema
+
+    }
+
+    "expand dataframe by uid and url" in {
+
+      implicit val spark: SparkSession = SparkSession.builder.config(sc.getConf).getOrCreate()
+
+      val logsLoader: WebLogsLoader = new WebLogsLoader()
+
+      val logsDF: DataFrame = logsLoader.getWebLogsDataFrame(s"${workingDir}/test")
+      val changedDF: DataFrame = logsLoader.processLogsDataFrame(logsDF)
+
+      changedDF.count() > 0 shouldBe true
+
+      val expectedSchema: StructType = StructType(
+        Seq(
+          StructField("uid", DataTypes.StringType),
+          StructField("url", DataTypes.StringType),
+          StructField("url_count", DataTypes.LongType, nullable = false)
+        )
+      )
+
+      changedDF.schema shouldBe expectedSchema
+      changedDF.show(20, 200, vertical = true)
+
+    }
 
   }
 
