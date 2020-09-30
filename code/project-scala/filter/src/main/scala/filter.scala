7593455b70ce1afdbedc7efb4bb6b9f9d4b419f4
Andrey Romanov <steampart@gmail.com>
2020-09-30 21:45:48 +0300

    lab04a fixes

--- code/project-scala/filter/src/main/scala/filter.scala
+++ code/project-scala/filter/src/main/scala/filter.scala
@@ -2,22 +2,47 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.Dataset
+
 object filter {
   def main(args: Array[String]): Unit = {
+
     val spark = SparkSession.builder().getOrCreate()
     import spark.implicits._
+
     val offset = spark.sparkContext.getConf.get("spark.filter.offset")
-    val df = spark.read.format("kafka").option("kafka.bootstrap.servers", "spark-master-1:6667").option("subscribe", spark.sparkContext.getConf.get("spark.filter.topic_name")).option("failOnDataLoss","false").option("startingOffsets",if (offset == "earliest") s"earliest" else s""" {"${spark.sparkContext.getConf.get("spark.filter.topic_name")}": {"0":$offset}}""").load()
+    val topicName = spark.sparkContext.getConf.get("spark.filter.topic_name")
+
+    val df = spark.read
+                  .format("kafka")
+                  .option("kafka.bootstrap.servers", "spark-master-1:6667")
+                  .option("subscribe", topicName)
+                  .option("failOnDataLoss","false")
+                  .option("startingOffsets",if (offset == "earliest") s"earliest" else s""" {"${topicName}": {"0":$offset}}""")
+                  .load()
+
     val json: Dataset[String] = df.select(col("value").cast("string")).as[String]
+
     val getData = udf { (timestamp: Long) =>
       val format = new java.text.SimpleDateFormat("yyyyMMdd")
       format.format(timestamp)
     }
-    val parsed = spark.read.json(json).select('category,'event_type,'item_id,'item_price,'timestamp, 'uid, getData('timestamp).as("date")).withColumn("date_rep",'date)
+
+    val parsed =
+      spark.read
+           .json(json)
+           .select('category,'event_type,'item_id,'item_price,'timestamp, 'uid, getData('timestamp).as("date"))
+           .withColumn("date_rep",'date)
+
     parsed.filter(col("event_type") === "view")
-      .write.partitionBy("date_rep").mode("overwrite").json(spark.sparkContext.getConf.get("spark.filter.output_dir_prefix") + "/view/")
+          .write
+          .partitionBy("date_rep")
+          .mode("overwrite").json(spark.sparkContext.getConf.get("spark.filter.output_dir_prefix") + "/view/")
+
     parsed.filter(col("event_type") === "buy")
-      .write.partitionBy("date_rep").mode("overwrite").json(spark.sparkContext.getConf.get("spark.filter.output_dir_prefix") + "/buy/")
+          .write
+          .partitionBy("date_rep")
+          .mode("overwrite").json(spark.sparkContext.getConf.get("spark.filter.output_dir_prefix") + "/buy/")
+
     spark.stop()
   }
 }
