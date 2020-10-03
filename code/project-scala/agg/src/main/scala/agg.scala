9a470d257c4b80740ef301ddd3a87f95218d9234
Andrey Romanov <steampart@gmail.com>
2020-10-03 14:43:27 +0300

    lab04b final variant

--- code/project-scala/agg/src/main/scala/agg.scala
+++ code/project-scala/agg/src/main/scala/agg.scala
@@ -2,6 +2,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.types.{DataTypes, StructType, TimestampType}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 
 object agg extends App {
 
@@ -26,9 +27,13 @@ object agg extends App {
 
   val rawDF: DataFrame = spark.readStream
     .format("kafka")
-    .option("kafka.bootstrap.servers", "spark-master-1:6667")
-    .option("subscribe", sourceTopic)
-    .option("maxOffsetsPerTrigger", "5000")
+    .options(
+      Map(
+        "kafka.bootstrap.servers" -> "spark-master-1:6667",
+        "subscribe"               -> sourceTopic,
+        "maxOffsetsPerTrigger"    -> "5000"
+      )
+    )
     .load
 
   val rawStringDF: DataFrame = rawDF.selectExpr("CAST(value AS STRING)")
@@ -39,5 +44,43 @@ object agg extends App {
 
   val rawDataChangedDF: DataFrame =
     rawDataDF.withColumn("timestamp_converted", (col("timestamp") / 1000).cast(TimestampType))
+
+  val aggregatedDF: DataFrame = rawDataChangedDF
+    .withWatermark("timestamp_converted", "1 hour")
+    .groupBy(window(col("timestamp_converted"), "1 hour", "1 hour"))
+    .agg(
+      sum(when(col("event_type") === "buy", col("item_price")).as("item_price_agg")).as("revenue"),
+      count(when(col("uid").isNotNull, true)).as("visitors"),
+      count(when(col("event_type") === "buy", true)).as("purchases")
+    )
+
+  val aggregatedChangedDF: DataFrame = aggregatedDF
+    .withColumn(
+      "aov",
+      col("revenue").cast(DataTypes.DoubleType) / col("purchases").cast(DataTypes.DoubleType)
+    )
+    .select(
+      unix_timestamp(col("window.start")).as("start_ts"),
+      unix_timestamp(col("window.end")).as("end_ts"),
+      col("revenue"),
+      col("visitors"),
+      col("purchases"),
+      col("aov")
+    )
+
+  aggregatedChangedDF.toJSON.writeStream
+    .format("kafka")
+    .trigger(Trigger.ProcessingTime("5 seconds"))
+    .options(
+      Map(
+        "kafka.bootstrap.servers" -> "10.0.0.5:6667",
+        "topic"                   -> sinkTopic,
+        "checkpointLocation"      -> s"chk/$checkpointDir"
+      )
+    )
+    .outputMode(OutputMode.Update())
+    .start()
+
+  spark.streams.awaitAnyTermination()
 
 }
