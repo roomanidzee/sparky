471ebdd2bb5491e185541df9df6db77fdd34d1ce
Andrey Romanov <steampart@gmail.com>
2020-10-24 12:55:31 +0300

    lab07 train job setup

--- code/project-scala/mlproject/src/main/scala/com/romanidze/sparky/mlproject/train/TrainingJob.scala
+++ code/project-scala/mlproject/src/main/scala/com/romanidze/sparky/mlproject/train/TrainingJob.scala
@@ -1,9 +1,28 @@
 package com.romanidze.sparky.mlproject.train
 
-import org.apache.spark.sql.SparkSession
+import com.romanidze.sparky.mlproject.train.weblogs.{DataLoader, PipelinePreparing}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class TrainingJob(implicit spark: SparkSession) {
 
-  def start(): Unit = {}
+  def start(): Unit = {
+
+    val datasetPath: String = spark.conf.get("spark.mlproject.dataset_path")
+    val modelSavePath: String = spark.conf.get("spark.mlproject.model_save_path")
+
+    val dataLoader = new DataLoader()
+    val logsRawDF: DataFrame = dataLoader.load(datasetPath)
+    val logsMLDF: DataFrame = dataLoader.convertDF(logsRawDF)
+
+    val pipeline: Pipeline = PipelinePreparing.getPipeline(logsMLDF)
+
+    val model: PipelineModel = pipeline.fit(logsMLDF)
+
+    model.write
+      .overwrite()
+      .save(modelSavePath)
+
+  }
 
 }
